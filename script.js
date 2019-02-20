4c1b6fd7a4326b10d643c44da0d2873dfc216474
Ruby Bantock <banruby@gmail.com>
2019-02-20 18:41:16 -0500

    Updated some functions.

--- script.js
+++ script.js
@@ -24,16 +24,24 @@ app.getSunTimes = () => {
     }).then((sunData) => {
         console.log(sunData);
         app.sunrise = sunData.results.sunrise;
+        app.sunset = sunData.results.sunset
         console.log(app.sunrise);
-        app.toEST();
+        app.updateHour(app.sunrise);
+        app.updateMinute(app.sunrise);
     });
 }
 
-app.toEST = () => {
-    const timeEST = app.sunrise.split(':');
-    console.log(timeEST);
-    const newEST = Number(timeEST[0]) - 5;
-    console.log(newEST * 60 + timeEST[1]);
+app.updateHour = (time) => {
+    let datePlaceholder = new Date (`February 20, 2019 ${time}`)
+    let currentHour = datePlaceholder.getHours();
+    currentHour = currentHour - 5;
+    console.log(currentHour);  
+}
+
+app.updateMinute = (time) => {
+    let datePlaceholder = new Date(`February 20, 2019 ${time}`)
+    let currentMinute = datePlaceholder.getMinutes();
+    console.log(currentMinute);
 }
 
 
@@ -42,3 +50,9 @@ $(document).ready(function(){
     app.getSunTimes();
     // app.toEST();
 }) //ends doc ready
+
+
+// OTHER STUFF
+
+// getting today time in the day in minutes
+    // console.log(newEST * 60 + timeEST[1]);
