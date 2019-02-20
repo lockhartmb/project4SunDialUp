d07ea3ed3faaed39d7ba51baabf39acd9e0e0218
Morgan <lockhart.mb@gmail.com>
2019-02-20 18:16:25 -0500

    made some stellar functions about time

--- script.js
+++ script.js
@@ -8,7 +8,37 @@ app.getUserTime = () => {
     return `{}`
 }
 
+app.getSunTimes = () => {
+    const longitude = -79.398003;
+    const latitude = 43.648190;
+    const url = 'https://api.sunrise-sunset.org/json';
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            lat: latitude,
+            lng: longitude,
+        }
+    }).then((sunData) => {
+        console.log(sunData);
+        app.sunrise = sunData.results.sunrise;
+        console.log(app.sunrise);
+        app.toEST();
+    });
+}
+
+app.toEST = () => {
+    const timeEST = app.sunrise.split(':');
+    console.log(timeEST);
+    const newEST = Number(timeEST[0]) - 5;
+    console.log(newEST * 60 + timeEST[1]);
+}
+
+
 // doc ready
 $(document).ready(function(){
-
+    app.getSunTimes();
+    // app.toEST();
 }) //ends doc ready
