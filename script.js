5919d55f926113aacd9f8bd9b97be3193402bee6
Morgan <lockhart.mb@gmail.com>
2019-02-20 20:01:36 -0500

    got more stuff organized, feelin a lot better now

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ app.currentTotalMinutes = (hours, minutes) => {
     return (hours * 60) + minutes;
 }
 
+// Takes the time and extracts the hours, then converts to EST by subtracting 5 hours, returns minutes
+app.convertToEST = (time) => {
+    let datePlaceholder = new Date(`February 20, 2019 ${time}`);
+    app.hourInEST = datePlaceholder.getHours();
+    app.hourInEST = app.hourInEST - 5;
+    return app.hourInEST;
+}
+
+// Takes the time and extracts the number of minutes, returns minutes
+app.updateMinute = (time) => {
+    datePlaceholder = new Date(`February 20, 2019 ${time}`);
+    app.updatedMinute = datePlaceholder.getMinutes();
+    // console.log(app.updatedMinute);
+    return app.updatedMinute;
+}
+
+// AJAX request to Sunrise/Sunset API
 app.getSunTimes = () => {
     const longitude = -79.398003;
     const latitude = 43.648190;
@@ -34,44 +51,41 @@ app.getSunTimes = () => {
         }
     }).then((sunData) => {
         app.sunrise = sunData.results.sunrise;
-        app.sunset = sunData.results.sunset
+        app.sunset = sunData.results.sunset;
+        // app.morningTwilight = sunData.results.civil_twilight_begin;
+        // app.eveningTwilight = sunData.results.civil_twilight_end;
         // console.log(app.sunrise);
         app.sunriseUpdate();
+        app.sunsetUpdate();
     });
 }
 
+// Takes the sunrise time from the API call, extracts hours and converts to EST, extracts minutes, then combines them into a total number of minutes
 app.sunriseUpdate = () => {
     app.sunriseHour = app.convertToEST(app.sunrise);
-    console.log(app.sunriseHour);
-    // app.sunriseMinute = app.updateMinute(app.sunrise);
-    // app.sunriseMinutes = app.currentTotalMinutes(app.sunriseHour, app.sunriseMinute);
-    // console.log(app.sunriseMinutes);
+    // console.log(app.sunriseHour);
+    app.sunriseMinute = app.updateMinute(app.sunrise);
+    app.sunriseMinutes = app.currentTotalMinutes(app.sunriseHour, app.sunriseMinute);
+    console.log(app.sunriseMinutes);
 }
 
-app.convertToEST = (time) => {
-    let datePlaceholder = new Date (`February 20, 2019 ${time}`);
-    app.hourInEST = datePlaceholder.getHours();
-    app.hourInEST = app.hourInEST - 5;
-    return app.hourInEST;
+app.sunsetUpdate = () => {
+    app.sunsetHour = app.convertToEST(app.sunset);
+    app.sunsetMinute = app.updateMinute(app.sunset);
+    app.sunsetMinutes = app.currentTotalMinutes(app.sunsetHour, app.sunsetMinute);
+    console.log(app.sunsetMinutes);
 }
 
-app.updateMinute = (time) => {
-    datePlaceholder = new Date(`February 20, 2019 ${time}`)
-    app.currentMinute = datePlaceholder.getMinutes();
-    // console.log(app.currentMinute);
-}
+app.currentTotalMinutes
 
 
-
-
-app.sunriseMinutes = app.currentTotalMinutes(app.currentHour, app.currentMinute);
 
 app.init = () => {
     app.getUserTime();
     app.getSunTimes();
-    
     app.currentTotalMinutes(4,7);
 }
+
 // doc ready
 $(document).ready(function(){
     app.init();
