9312ee9faf15a087c0a19e335bb386ba2b4fb983
Morgan <lockhart.mb@gmail.com>
2019-02-20 19:01:31 -0500

    we are starting to make it more organized

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
 const app = {};
 
+// Get user time, which is already in 24 hours and divide into hours and minutes. Then convert into a total number of user minutes
 app.getUserTime = () => {
-    app.hours = getHours();
-    app.minutes = getMinutes();
-    app.seconds = getSeconds();
-    return `{}`
+    const today = new Date();
+    app.hours = today.getHours();
+    // console.log(app.hours);
+    app.minutes = today.getMinutes();
+    // console.log(app.minutes);
+    app.seconds = today.getSeconds();
+
+    app.userMinutes = app.currentTotalMinutes(app.hours, app.minutes);
+    // console.log(app.userMinutes);
 }
+
+
+// Takes number of hours and number of minutes and converts to one total number of minutes
+app.currentTotalMinutes = (hours, minutes) => {
+    return (hours * 60) + minutes;
+}
+
+
 
 app.getSunTimes = () => {
     const longitude = -79.398003;
@@ -28,27 +42,39 @@ app.getSunTimes = () => {
         console.log(app.sunrise);
         app.updateHour(app.sunrise);
         app.updateMinute(app.sunrise);
+        
     });
 }
 
-app.updateHour = (time) => {
-    let datePlaceholder = new Date (`February 20, 2019 ${time}`)
-    let currentHour = datePlaceholder.getHours();
-    currentHour = currentHour - 5;
-    console.log(currentHour);  
+app.updateHour = () => {
+    let datePlaceholder = new Date (`February 20, 2019 ${app.sunrise}`)
+    app.currentHour = datePlaceholder.getHours();
+    app.currentHour = app.currentHour - 5;
+    console.log(app.currentHour);
 }
 
 app.updateMinute = (time) => {
     let datePlaceholder = new Date(`February 20, 2019 ${time}`)
-    let currentMinute = datePlaceholder.getMinutes();
-    console.log(currentMinute);
+    app.currentMinute = datePlaceholder.getMinutes();
+    console.log(app.currentMinute);
 }
 
 
+
+
+app.sunriseMinutes = app.currentTotalMinutes(app.currentHour, app.currentMinute);
+
+app.init = () => {
+    app.getUserTime();
+    app.getSunTimes();
+    app.updateHour();
+    app.updateMinute();
+    app.currentTotalMinutes(4,7);
+}
 // doc ready
 $(document).ready(function(){
-    app.getSunTimes();
-    // app.toEST();
+    app.init();
+    
 }) //ends doc ready
 
 
