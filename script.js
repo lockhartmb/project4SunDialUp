956a793ac473b60d5a067cfb401f3342feb7038b
Ruby Bantock <banruby@gmail.com>
2019-02-21 20:33:37 -0500

    Testing more options for the first colorscape.

--- script.js
+++ script.js
@@ -61,6 +61,8 @@ app.getSunTimes = () => {
         app.sunsetUpdate();
         app.noonUpdate();
         app.displayColors();
+        app.createColorArrays();
+        // loadUserTimeColors();
     });
 }
 
@@ -87,6 +89,98 @@ app.noonUpdate = () => {
     console.log(app.noonMinutes);
 }
 
+app.createColorArrays = () => {
+    app.topColors = chroma
+        .scale(["rgb(0,0,0)", "rgb(227,116,58)", "rgb(116, 228, 238)", "rgb(104,62,233)", "rgb(0,0,0)"])
+        // .mode("lch")
+        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
+        .colors(1440);
+
+    app.bottomColors = chroma
+        .scale(["rgb(74,71,71)", "rgb(238,196,30)", "rgb(255,255,255)", "rgb(236,183,226)", "rgb(74,71,71)"])
+        // .mode("lch")
+        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
+        .colors(1440);
+}
+
+app.createPairedArrays = (array1, array2) => {
+    app.colorPairs = [];
+    array1.map((color, i) => {
+        let newPair = []
+        newPair.push(color, array2[i])
+        app.colorPairs.push(newPair)
+    })
+    app.colorPairs.forEach((colorPair, index) => {
+        setTimeout(function () {
+            $("main").css({ background: `linear-gradient(${colorPair[0]}, ${colorPair[1]})` });
+            console.log(`${colorPair[0]}`)
+        }, index * app.interval);
+    })
+}
+
+app.displayColors = () => {
+    app.topColors = chroma
+        .scale(["rgb(0,0,0)", "rgb(227,116,58)", "rgb(116, 228, 238)", "rgb(104,62,233)", "rgb(0,0,0)"])
+        // .mode("lch")
+        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
+        .colors(1440);
+
+    app.bottomColors = chroma
+        .scale(["rgb(74,71,71)", "rgb(238,196,30)", "rgb(255,255,255)", "rgb(236,183,226)", "rgb(74,71,71)"])
+        // .mode("lch")
+        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
+        .colors(1440);
+
+    // const colorPairs = [];
+    // topColors.map((color, i) => {
+    //     // console.log(color, i)
+    //     let newPair = []
+    //     newPair.push(color, bottomColors[i])
+    //     // console.log(newPair)
+    //     colorPairs.push(newPair)
+    // });
+
+    // colorPairs.forEach(colorPair => {
+    // console.log(`linear-gradient: to top, ${colorPair[0]}, ${colorPair[1]}`)
+    // $("main").css("background-image", `linear-gradient: to top, ${colorPair[0]}, ${colorPair[1]}`);
+    // console.log(colorPairs);
+    // });
+
+    app.interval = 10; //this will need to be 60,000 to be real time
+
+    // const colorTest = colorPairs.forEach((colorPair, index) => {
+    //     setTimeout(function () {
+    //         color = chroma(color).css();
+    //         $("main").css({
+    //             // background: `-webkit-linear-gradient(to top, ${colorPair[0]}, ${colorPair[1]})`
+    //         });
+    //         // console.log(color);
+    //     }, index * interval);
+    // });
+
+    // const colorTest = colorPairs.forEach((colorPair, index) => {
+    //     setTimeout(function () {
+    //         colorPair = chroma(colorPair).css();
+    //         $("main").css("background-color", colorPair[0]);
+    //         console.log(colorPair);
+    //     }, index * interval);
+    // });
+
+
+    // WE MAY NOT NEED THIS ANYMORE SINCE IT STILL WORKS WITHOUT IT
+    // const arrayFormat = (array) => {
+    //     array.forEach((color) => {
+    //         color = chroma(color).css();
+    //     });
+    // }
+
+    // it still works without this? do we still need it?
+    // arrayFormat(topColors);
+    // arrayFormat(bottomColors);
+
+    app.createPairedArrays(app.topColors, app.bottomColors);
+    
+}
 
 
 
@@ -110,85 +204,62 @@ $(document).ready(function(){
 
 // COLOR STUFF
 
-
-app.displayColors = () => {
-    const topColors = chroma
-        .scale(["rgb(0,0,0)", "rgb(227,116,58)", "rgb(116, 228, 238)", "rgb(104,62,233)", "rgb(0,0,0)"])
-        // .mode("lch")
-        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
-        .colors(1440);
-
-    const bottomColors = chroma
-    .scale(["rgb(74,71,71)", "rgb(238,196,30)", "rgb(255,255,255)", "rgb(236,183,226)", "rgb(74,71,71)"])
-    // .mode("lch")
-        .domain([0, `${app.sunriseMinutes}`, `${app.noonMinutes}`, `${app.sunsetMinutes}`, 1440])
-    .colors(1440);
-
-    // const colorPairs = [];
-    // topColors.map((color, i) => {
-    //     // console.log(color, i)
-    //     let newPair = []
-    //     newPair.push(color, bottomColors[i])
-    //     // console.log(newPair)
-    //     colorPairs.push(newPair)
-    // });
-
-    // colorPairs.forEach(colorPair => {
-        // console.log(`linear-gradient: to top, ${colorPair[0]}, ${colorPair[1]}`)
-        // $("main").css("background-image", `linear-gradient: to top, ${colorPair[0]}, ${colorPair[1]}`);
-        // console.log(colorPairs);
-    // });
-
-    const interval = 10; //this will need to be 60,000 to be real time
-
-    // const colorTest = colorPairs.forEach((colorPair, index) => {
-    //     setTimeout(function () {
-    //         color = chroma(color).css();
-    //         $("main").css({
-    //             // background: `-webkit-linear-gradient(to top, ${colorPair[0]}, ${colorPair[1]})`
-    //         });
-    //         // console.log(color);
-    //     }, index * interval);
-    // });
-
-    // const colorTest = colorPairs.forEach((colorPair, index) => {
-    //     setTimeout(function () {
-    //         colorPair = chroma(colorPair).css();
-    //         $("main").css("background-color", colorPair[0]);
-    //         console.log(colorPair);
-    //     }, index * interval);
-    // });
-
-    const arrayFormat = (array) => {
-        array.forEach((color) => {
-            color = chroma(color).css();
-        });
-    }
-    arrayFormat(topColors);
-    console.log(topColors);
-    arrayFormat(bottomColors);
-
-    const colorPairs = [];
-    topColors.map((color, i) => {
-        // console.log(color, i)
-        let newPair = []
-        newPair.push(color, bottomColors[i])
-        // console.log(newPair)
-        colorPairs.push(newPair)
-    })
-    colorPairs.forEach((colorPair, index) => {
-        setTimeout(function() {
-            // console.log(colorPair);
-            // console.log(`linear-gradient:${colorPair[0]} ${colorPair[1]}`)
-            // $("main").css("background-image", `linear-gradient${colorPair[0]}`);
-            $("main").css({ background: `linear-gradient(${colorPair[0]}, ${colorPair[1]})`});
-        }, index * interval);
-    })
-    
-
+const arraySlice = (array) => {
+    return array.slice(`${app.userMinutes}`);
 }
 
-// FROM ZOE!
 
-// const colors1 = ['red', 'orange', 'yellow', 'green']
-// const colors2 = ['pink', 'blue', 'white', 'purple']
+
+
+
+
+
+// loadUserTimeColors = () => {
+    
+
+//     //slice the top and bottom color arrays to just include values remaining in the current user's day
+//     app.choppedTopColors = arraySlice(app.topColors);
+//     app.choppedBottomColors = arraySlice(app.bottomColors);
+
+//     // uses the length of the array to find the final value in the array to be iterated through
+//     app.choppedFinalValue = app.choppedTopColors.length;
+    
+//     // calculates updates domain values
+//     app.choppedSunriseMinutes = app.sunriseMinutes - app.userMinutes;
+//     app.choppedNoonMinutes = app.noonMinutes - app.userMinutes;
+//     app.choppedSunsetMinutes = app.sunsetMinutes - app.userMinutes;
+
+    
+
+//     if (app.userMinutes <= app.sunriseMinutes) {
+//         console.log("BEFORE SUNRISE");
+
+//         app.choppedTopColors = chroma
+//             .scale([`${app.choppedTopColors[0]}`, "rgb(227,116,58)", "rgb(116, 228, 238)", "rgb(104,62,233)", "rgb(0,0,0)"])
+//             .domain([0, `${app.choppedSunriseMinutes}`, `${app.choppedNoonMinutes}`, `${app.choppedSunsetMinutes}`, `${app.choppedFinalValue}`])
+//             .colors(`${app.choppedFinalValue}`);
+
+//         app.choppedBottomColors = chroma
+//             .scale([`${app.choppedTopColors[0]}`, "rgb(227,116,58)", "rgb(116, 228, 238)", "rgb(104,62,233)", "rgb(0,0,0)"])
+//             .domain([0, `${app.choppedSunriseMinutes}`, `${app.choppedNoonMinutes}`, `${app.choppedSunsetMinutes}`, `${app.choppedFinalValue}`])
+//             .colors(`${app.choppedFinalValue}`);
+
+//         app.createPairedArrays(app.topColors, app.bottomColors);
+
+//     } else if (app.userMinutes <= app.noonMinutes) {
+//         console.log("BEFORE NOON");
+//     } else if (app.userMinutes <= app.sunsetMinutes) {
+//         console.log("BEFORE SUNSET");
+//     } else if (app.userMinutes > app.sunsetMinutes) {
+//         console.log("IT'S EVENING, YO!");
+
+//         app.choppedTopColors = chroma
+//             .scale([`${app.choppedTopColors[0]}`, "rgb(0,0,0)"])
+//             .domain([0, `${app.choppedFinalValue}`])
+//             .colors(`${app.choppedFinalValue}`);
+
+//         app.createPairedArrays(app.choppedTopColors, app.choppedBottomColors);
+//     }
+// }
+
+
